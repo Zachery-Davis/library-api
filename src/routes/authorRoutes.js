21376c9910b57213f532d937b05fe57b0cd258a8
pwonders-801 <pwonders@charlotte.edu>
2025-11-23 17:24:18 -0500

    added routes

--- src/routes/authorRoutes.js
+++ src/routes/authorRoutes.js
@@ -1,1 +1,13 @@
+import express from 'express';
+import * as authorController from '../controllers/authorController.js';
+import { validateAuthor } from '../middleware/authorValidator.js';
 
+const router = express.Router();
+
+router.post('/', validateAuthor, authorController.createAuthor);
+router.get('/', authorController.getAuthors);
+router.get('/:id', authorController.getAuthor);
+router.put('/:id', validateAuthor, authorController.updateAuthor);
+router.delete('/:id', authorController.deleteAuthor);
+
+export default router;
