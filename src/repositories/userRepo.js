06ba9dc76131a0352f012772d60678a8a6cb1a88
pwonders-801 <pwonders@charlotte.edu>
2025-11-23 17:39:52 -0500

    changed

--- src/repositories/userRepo.js
+++ src/repositories/userRepo.js
@@ -4,16 +4,16 @@ export default {
   async getAllUsers() {
     return prisma.user.findMany();
   },
-  async getUserById(id) {
-    return prisma.user.findUnique({ where: { id } });
+  async getUserById(userId) {
+    return prisma.user.findUnique({ where: { userId } });
   },
   async createUser(data) {
     return prisma.user.create({ data });
   },
-  async updateUser(id, data) {
-    return prisma.user.update({ where: { id }, data });
+  async updateUser(userId, data) {
+    return prisma.user.update({ where: { userId }, data });
   },
-  async deleteUser(id) {
-    return prisma.user.delete({ where: { id } });
+  async deleteUser(userId) {
+    return prisma.user.delete({ where: { userId } });
   },
 };
