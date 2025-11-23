4ec20209037291f326baf2152c7d4e7c5c967681
pwonders-801 <pwonders@charlotte.edu>
2025-11-23 17:43:15 -0500

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,26 @@
+import userService from '../services/userService.js';
 
+export async function createUser(req, res) {
+  const created = await userService.createUser(req.body);
+  res.status(201).json(created);
+}
+
+export async function getUsers(req, res) {
+  const users = await userService.getAllUsers();
+  res.status(200).json(users);
+}
+
+export async function getUser(req, res) {
+  const user = await userService.getUserById(req.params.id);
+  res.status(200).json(user);
+}
+
+export async function updateUser(req, res) {
+  const updated = await userService.updateUser(req.params.id, req.body);
+  res.status(200).json(updated);
+}
+
+export async function deleteUser(req, res) {
+  const deleted = await userService.deleteUser(req.params.id);
+  res.status(200).json(deleted);
+}
