c98d8e21bcfeac33f5c4037ccde3bac425b78850
pwonders-801 <pwonders@charlotte.edu>
2025-11-23 17:41:50 -0500

    Update userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,1 +1,23 @@
+import userRepo from '../repositories/userRepo.js';
 
+export default {
+  async createUser(data) {
+    return userRepo.createUser(data);
+  },
+
+  async getUserById(id) {
+    return userRepo.getUserById(Number(id));
+  },
+
+  async getAllUsers() {
+    return userRepo.getAllUsers();
+  },
+
+  async updateUser(id, data) {
+    return userRepo.updateUser(Number(id), data);
+  },
+
+  async deleteUser(id) {
+    return userRepo.deleteUser(Number(id));
+  },
+};
