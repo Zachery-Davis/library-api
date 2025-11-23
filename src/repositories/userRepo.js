2ee1df13fc16e696a146779827a363b6a8add9bf
pwonders-801 <pwonders@charlotte.edu>
2025-11-23 18:24:56 -0500

    updated to include relational fields

--- src/repositories/userRepo.js
+++ src/repositories/userRepo.js
@@ -2,10 +2,37 @@ import prisma from '../config/db.js';
 
 export default {
   async getAllUsers() {
-    return prisma.user.findMany();
+    return prisma.user.findMany({
+      include: {
+        checkouts: {
+          include: {
+            book: {
+              include: {
+                bookAuthors: { include: { author: true } },
+                bookGenres: { include: { genre: true } },
+              },
+            },
+          },
+        },
+      },
+    });
   },
   async getUserById(userId) {
-    return prisma.user.findUnique({ where: { userId } });
+    return prisma.user.findUnique({
+      where: { userId },
+      include: {
+        checkouts: {
+          include: {
+            book: {
+              include: {
+                bookAuthors: { include: { author: true } },
+                bookGenres: { include: { genre: true } },
+              },
+            },
+          },
+        },
+      },
+    });
   },
   async createUser(data) {
     return prisma.user.create({ data });
