a5d4c9059d861c919608cecb2626090cf3415159
pwonders-801 <pwonders@charlotte.edu>
2025-11-23 18:20:58 -0500

    updated to include relational fields

--- src/repositories/authorRepo.js
+++ src/repositories/authorRepo.js
@@ -5,10 +5,39 @@ export default {
     return prisma.author.create({ data });
   },
   async getAuthorById(authorId) {
-    return prisma.author.findUnique({ where: { authorId } });
+    return prisma.author.findUnique({
+      where: { authorId },
+      include: {
+        bookAuthors: {
+          include: {
+            book: {
+              include: {
+                bookGenres: { include: { genre: true } },
+                bookAuthors: { include: { author: true } },
+                checkouts: { include: { user: true } },
+              },
+            },
+          },
+        },
+      },
+    });
   },
   async getAllAuthors() {
-    return prisma.author.findMany();
+    return prisma.author.findMany({
+      include: {
+        bookAuthors: {
+          include: {
+            book: {
+              include: {
+                bookGenres: { include: { genre: true } },
+                bookAuthors: { include: { author: true } },
+                checkouts: { include: { user: true } },
+              },
+            },
+          },
+        },
+      },
+    });
   },
   async updateAuthor(authorId, data) {
     return prisma.author.update({ where: { authorId }, data });
