b91e2a3d3aa4456e5f6c677de15aec4080fcc290
pwonders-801 <pwonders@charlotte.edu>
2025-11-23 17:16:53 -0500

    added matching functions from repo

--- src/services/authorService.js
+++ src/services/authorService.js
@@ -1,1 +1,23 @@
+import authorRepo from '../repositories/authorRepo.js';
 
+export default {
+  async createAuthor(data) {
+    return authorRepo.createAuthor(data);
+  },
+
+  async getAuthorById(id) {
+    return authorRepo.getAuthorById(Number(id));
+  },
+
+  async getAllAuthors() {
+    return authorRepo.getAllAuthors();
+  },
+
+  async updateAuthor(id, data) {
+    return authorRepo.updateAuthor(Number(id), data);
+  },
+
+  async deleteAuthor(id) {
+    return authorRepo.deleteAuthor(Number(id));
+  },
+};
