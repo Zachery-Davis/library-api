a32f9b8c7f94687ff5f94a104751cba627bdf551
Zachery Davis <zacherydavis@hotmail.com>
2025-11-24 19:48:57 -0500

    Merge Fixes

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import * as bookController from '../controllers/bookController.js';
-import * as bookValidator from '../middleware/bookValidator.js';
+import { validateBook } from '../middleware/bookValidator.js';
 
 const router = express.Router();
 
 router.get('/', bookController.getBooks);
-router.post('/', bookValidator, bookController.createBook);
+router.post('/', validateBook, bookController.createBook);
 router.get('/:id', bookController.getBook);
-router.put('/:id', bookValidator, bookController.updateBook);
+router.put('/:id', validateBook, bookController.updateBook);
 router.delete('/:id', bookController.deleteBook);
 
 export default router;
